9f344110f61d99a25e4db9d803d917948888d022
masrafi143 <masrafiofficial143@gmail.com>
2025-11-18 18:19:52 +0600

    initialize project & add navbar & footer

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../pages/Shared/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../pages/Shared/Footer';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default RootLayout;
